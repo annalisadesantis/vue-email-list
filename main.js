db725a1be2955d69a76bf9a3fe0702f88737c040
Annalisa <annalisadesantis770@gmail.com>
2020-11-20 14:39:48 +0100

    stampa 10 email in pagina

--- main.js
+++ main.js
@@ -3,10 +3,20 @@
 var app = new Vue({
     el: "#root",
     data: {
-
-
+        email: []
     },
-    methods: {
+    mounted() {
+
+        for (var i = 0; i < 10; i++) {
+            const self = this;
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(function(response) {
+                    console.log(response.data);
+                    self.email.push(response.data.response);
+
+                });
+        }
 
 
     }
