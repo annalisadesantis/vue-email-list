18a996f9ae9a27e8c6f5ef27bfa5c676d6e1e6de
Annalisa <annalisadesantis770@gmail.com>
2020-11-20 14:27:48 +0100

    creazione file

---
+++ main.js
@@ -0,0 +1,14 @@
+// Tramite l'API https://flynn.boolean.careers/exercises/api/random/mail generare una lista di 10 indirizzi email e stamparle in pagina con vue.
+
+var app = new Vue({
+    el: "#root",
+    data: {
+
+
+    },
+    methods: {
+
+
+    }
+
+});
